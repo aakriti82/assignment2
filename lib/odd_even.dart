9ce3a0055240919176ee86ea278ce4a7c25509ab
aakriti82 <aakritiyadav82@gmail.com>
2024-11-20 00:25:30 +0545

    vowel_consonent

---
+++ lib/odd_even.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Predefined number to check
+  int number = 15; // You can change this number to any other integer
+
+  // Check if the number is even or odd
+  if (number % 2 == 0) {
+    print('$number is even');
+  } else {
+    print('$number is odd');
+  }
+}
+
+
+
+
